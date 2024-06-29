cd2719364ea6cb295a639b7240ca846cf93fd51d
cubeSugar <wxbcly@163.com>
2024-06-29 12:14:14 +0800

    lab4

--- kern/syscall_all.c
+++ kern/syscall_all.c
@@ -459,22 +459,7 @@ int sys_cgetc(void) {
  */
 int sys_write_dev(u_int va, u_int pa, u_int len) {
 	/* Exercise 5.1: Your code here. (1/2) */
-	if (is_illegal_va_range(va, len)) return -E_INVAL;
-	if (!((pa >= 0x180003f8 && pa + len <= 0x18000418) || (pa >= 0x180001f0 && pa + len <= 0x180001f8))) {
-		return -E_INVAL;
-	}
-	if (len == 1) {
-		uint8_t data = *((volatile uint8_t *)va);
-		iowrite8(data, pa);
-	} else if (len == 2) {
-		uint16_t data = *((volatile uint16_t *)va);
-		iowrite16(data, pa);
-	} else if (len == 4) {
-		uint32_t data = *((volatile uint32_t *)va);
-		iowrite32(data, pa);
-	} else {
-		return -E_INVAL;
-	}
+
 	return 0;
 }
 
@@ -495,22 +480,7 @@ int sys_write_dev(u_int va, u_int pa, u_int len) {
  */
 int sys_read_dev(u_int va, u_int pa, u_int len) {
 	/* Exercise 5.1: Your code here. (2/2) */
-	if (is_illegal_va_range(va, len)) return -E_INVAL;
-	if (!((pa >= 0x180003f8 && pa + len <= 0x18000418) || (pa >= 0x180001f0 && pa + len <= 0x180001f8))) {
-		return -E_INVAL;
-	}
-	if (len == 1) {
-		uint8_t data = ioread8(pa);
-		*((volatile uint8_t *)va) = data;
-	} else if (len == 2) {
-		uint16_t data = ioread16(pa);
-		*((volatile uint16_t *)va) = data;
-	} else if (len == 4) {
-		uint32_t data = ioread32(pa);
-		*((volatile uint32_t *)va) = data;
-	} else {
-		return -E_INVAL;
-	}
+
 	return 0;
 }
 
